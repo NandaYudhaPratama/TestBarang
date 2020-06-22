cd567f32409ecbfe91806f0563075ac8e4b2365e
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-06-22 20:08:26 +0700

    Add Edit Delete

--- app/src/main/java/com/example/testbarang/AdapterLihatBarang.java
+++ app/src/main/java/com/example/testbarang/AdapterLihatBarang.java
@@ -1,29 +1,28 @@
 package com.example.testbarang;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.content.Context;
 import java.util.ArrayList;
 import androidx.recyclerview.widget.RecyclerView;
+
 public class AdapterLihatBarang extends
         RecyclerView.Adapter<AdapterLihatBarang.ViewHolder> {
     private ArrayList<Barang> daftarBarang;
     private Context context;
+    FirebaseDataListener listener;
+
     public AdapterLihatBarang(ArrayList<Barang> barangs, Context ctx){
-        /**
-         * Inisiasi data dan variabel yang akan digunakan
-         */
         daftarBarang = barangs;
         context = ctx;
+        listener = (LihatBarang)ctx;
     }
     class ViewHolder extends RecyclerView.ViewHolder {
-        /**
-         * Inisiasi View
-         * Disini kita hanya menggunakan data String untuk tiap item
-         * dan juga view nya hanyalah satu TextView
-         */
         TextView tvTitle;
         ViewHolder(View v) {
             super(v);
@@ -32,36 +31,49 @@ public class AdapterLihatBarang extends
     }
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        /**
-         * Inisiasi ViewHolder
-         */
         View v =
-                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent,
-                        false);
-        // mengeset ukuran view, margin, padding, dan parameter layout lainnya
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.item_barang, parent, false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        /**
-         * Menampilkan data pada view
-         */
         final String name = daftarBarang.get(position).getNama();
         holder.tvTitle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /**
-                 * untuk latihan Selanjutnya , jika ingin membaca detail data
-                 */
             }
         });
         holder.tvTitle.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                /**
-                 * untuk latihan Selanjutnya ,fungsi Delete dan Update data
-                 */
+                final Dialog dialog = new Dialog(context);
+                dialog.setContentView(R.layout.dialog);
+                dialog.setTitle("Pilih Aksi");
+                dialog.show();
+
+                Button editButton = (Button) dialog.findViewById(R.id.bt_edit_data);
+                Button delButton = (Button) dialog.findViewById(R.id.bt_delete_data);
+
+                editButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                                context.startActivity(TambahData.getActIntent((Activity) context).putExtra("data", daftarBarang.get(position)));
+                            }
+                        }
+                );
+
+                delButton.setOnClickListener(
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                                listener.onDeleteData(daftarBarang.get(position), position);
+                            }
+                        }
+                );
                 return true;
             }
         });
@@ -69,9 +81,10 @@ public class AdapterLihatBarang extends
     }
     @Override
     public int getItemCount() {
-        /**
-         * Mengembalikan jumlah item pada barang
-         */
         return daftarBarang.size();
+    }
+
+    public interface FirebaseDataListener{
+        void onDeleteData(Barang barang, int position);
     }
 }
--- app/src/main/java/com/example/testbarang/TambahData.java
+++ app/src/main/java/com/example/testbarang/TambahData.java
@@ -2,10 +2,10 @@ package com.example.testbarang;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
@@ -21,7 +21,7 @@ public class TambahData extends AppCompatActivity {
 
     private Button btSubmit;
     private EditText etKode;
-    private EditText etnama;
+    private EditText etNama;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,41 +29,70 @@ public class TambahData extends AppCompatActivity {
         setContentView(R.layout.activity_tambah_data);
 
         etKode = (EditText) findViewById(R.id.editNo);
-        etnama = (EditText) findViewById(R.id.editNama);
+        etNama = (EditText) findViewById(R.id.editNama);
         btSubmit = (Button) findViewById(R.id.btnOk);
 
         database = FirebaseDatabase.getInstance().getReference();
 
-        btSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!(etKode.getText().toString().isEmpty())&&
-                        !(etnama.getText().toString().isEmpty()))
-                    submitBrg(new Barang(etKode.getText().toString(),
-                            etnama.getText().toString()));
-                else
-                    Toast.makeText(getApplicationContext(),"Data tidak boleh kosong",
-                            Toast.LENGTH_LONG).show();
+        final Barang barang = (Barang) getIntent().getSerializableExtra("data");
 
-                InputMethodManager imm = (InputMethodManager)
-                        getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(etKode.getWindowToken(),0);
-            }
-        });
+        if (barang != null){
+            etKode.setText(barang.getKode());
+            etNama.setText(barang.getNama());
+            btSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    barang.setKode(etKode.getText().toString());
+                    barang.setNama(etNama.getText().toString());
+                    updateBarang(barang);
+                }
+            });
+        }else {
+            btSubmit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (!(etKode.getText().toString().isEmpty()) &&
+                            !(etNama.getText().toString().isEmpty()))
+                        submitBrg(new Barang(etKode.getText().toString(),
+                                etNama.getText().toString()));
+                    else
+                        Toast.makeText(getApplicationContext(), "Data tidak boleh Kosong",
+                                Toast.LENGTH_LONG).show();
+
+                    InputMethodManager imm = (InputMethodManager)
+                            getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(etKode.getWindowToken(), 0);
+                }
+            });
+        }
     }
-    public void submitBrg (Barang brg){
+    public void submitBrg(Barang brg){
         database.child("Barang").push().setValue(brg).addOnSuccessListener(this,
                 new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
                         etKode.setText("");
-                        etnama.setText("");
-                        Toast.makeText(getApplicationContext(),"Data berhasil ditambahkan",
+                        etNama.setText("");
+                        Toast.makeText(getApplicationContext(),"Data berhasil ditambahkan" ,
                                 Toast.LENGTH_LONG).show();
                     }
                 });
     }
     public static Intent getActIntent(Activity activity){
         return new Intent(activity, TambahData.class);
+    }
+
+    private void updateBarang(Barang brg) {
+        database.child("Barang")
+                .child(brg.getKode())
+                .setValue(brg)
+                .addOnSuccessListener(this, new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(getApplicationContext(), "Data berhasil di Update",
+                                Toast.LENGTH_LONG).show();
+                        finish();
+                    }
+                });
     }
 }
